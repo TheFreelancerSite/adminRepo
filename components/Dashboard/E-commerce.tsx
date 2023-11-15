af716edb70905d69fa745f9b94038ba07a711df2
Haithemch98 <chaouchhaithem0@gmail.com>
2023-11-15 23:47:30 +0100

    add a chart of clients using our paltform

--- components/Dashboard/E-commerce.tsx
+++ components/Dashboard/E-commerce.tsx
@@ -1,18 +1,13 @@
 "use client";
 import React from "react";
-import ChartOne from "../Charts/ChartOne";
-import ChartThree from "../Charts/ChartThree";
-import ChartTwo from "../Charts/ChartTwo";
-import ChatCard from "../Chat/ChatCard";
-import TableOne from "../Tables/ClientsTable";
+import ChartOne from "../../components/Charts/charts";
+// import ChartThree from "../Charts/ChartThree";
+// import ChartTwo from "../Charts/ChartTwo";
+
 import CardDataStats from "../CardDataStats";
-// import Map from "../Maps/TestMap";
 
 // without this the component renders on server and throws an error
-import dynamic from "next/dynamic";
-const MapOne = dynamic(() => import("../Maps/MapOne"), {
-  ssr: false,
-});
+
 
 const ECommerce: React.FC = () => {
   return (
@@ -106,14 +101,13 @@ const ECommerce: React.FC = () => {
       </div>
 
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
-        <ChartOne />
-        <ChartTwo />
-        <ChartThree />
-        <MapOne />
-        <div className="col-span-12 xl:col-span-8">
-          <TableOne />
-        </div>
-        <ChatCard />
+        { <ChartOne />
+        /*<ChartTwo />
+        <ChartThree /> */}
+        
+       
+       
+        
       </div>
     </>
   );
